624d7962d6dab132958af76362fe4a9142ff4cf1
Yong ZHANG <zhangyong199010@gmail.com>
2018-12-05 10:18:04 +0800

    Update filter_variants.py

--- filter_variants.py
+++ filter_variants.py
@@ -5,14 +5,19 @@
 
 
 import pandas as pd
+import argparse
 
 
-variants_orig = pd.read_csv('data/variants_Alex_orig', sep='\t')
+parser = argparse.ArgumentParser(description='Standardize vcf form and filter variants based on P-value')
+parser.add_argument('inFile', type=str, action='store', dest='inFile', help='Path of the input file')
+parser.add_argument('outFile', type=str, action='store', dest='outFile', help='Path of the output file')
+args = parser.parse_args()
+# args = parser.parse_args('data/variants_Alex_orig data/variants_0.001.vcf'.split())
+
+variants_orig = pd.read_csv(args.inFile, sep='\t')
 variants_orig = variants_orig[variants_orig.PVALUE < 0.001]
 variants_orig['chr'] = 'chr'
 variants_orig['CHROM'] = variants_orig.CHROM.astype(str)
-# print(variants_orig.dtypes)
 variants_orig['CHROM'] = variants_orig['chr'] + variants_orig['CHROM']
 variants_orig['-'] = '-'
-variants_orig.to_csv('data/variants_0.001.vcf', columns=['CHROM', 'POS', '-', 'REF', 'ALT'], header=False, index=False,
-                     sep='\t')
+variants_orig.to_csv(args.outFile, columns=['CHROM', 'POS', '-', 'REF', 'ALT'], header=False, index=False, sep='\t')
