1e30cf4d4c6ef3b6831434a3a1f45e7f5a02b108
zhangyonglzlm <zhangyong199010@gmail.com>
2018-12-14 12:34:07 +0800

    fix variants_0.001_0based.vcf.bed.sorted.bed.closestgene_RefAlt bugs

--- preprocess_closest-features_addRefAlt.py
+++ preprocess_closest-features_addRefAlt.py
@@ -8,13 +8,17 @@ import argparse
 parser = argparse.ArgumentParser(description='Help add REF and ALT columns')
 parser.add_argument('inFile', type=str, action='store',
                     help='Path of the file without REF and ALT columns')
+
+parser.add_argument('RefAlt', type=str, action='store',
+                    help='Path of the file with REF and ALT information')
+
 parser.add_argument('outFile', type=str, action='store',
                     help='Path of the file with REF and ALT columns')
 args = parser.parse_args()
 
-vcf = pd.read_table(args.inFile, header=None, names=['CHROM', 'POS', '-', 'REF', 'ALT'])
-closestgene = pd.read_table('data/variants_0.001_for_closest-features.vcf.bed.sorted.bed.closestgene', header=None,
-                            names=['CHROM', 'POS_0', 'POS', 'chr', 'TSS_0', 'TSS', 'Strand', 'Gene_ID', 'Dist'])
+closestgene = pd.read_table(args.inFile, header=None, names=['CHROM', 'POS_0', 'POS', 'chr', 'TSS_0', 'TSS', 'Strand', 'Gene_ID', 'Dist'])
+vcf = pd.read_table(args.RefAlt, header=None, names=['CHROM', 'POS', '-', 'REF', 'ALT'])
+
 merged = pd.merge(closestgene, vcf, on=['CHROM', 'POS'], how='outer')
 merged = merged.reindex(columns=['CHROM', 'POS_0', 'POS', 'REF', 'ALT', 'chr', 'TSS_0', 'TSS', 'Strand', 'Gene_ID', 'Dist'])
 order_custom = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12',
